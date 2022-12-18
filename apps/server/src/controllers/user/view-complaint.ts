b2aa058aad0eb23dab03c1b2cb5fc9850b3248f1
Youssef Saad <youssef.elbasha.saad@gmail.com>
2022-12-18 19:10:30 +0200

    Implemented view complaints controller

---
+++ apps/server/src/controllers/user/view-complaint.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express'
+import Customer from '../../types/users/customer';
+import CustomerService from '../../types/users/customer-service';
+
+const viewComplaint = async (req: Request, res: Response) => {
+  try {
+
+    const role = req.user?.role?.toUpperCase()
+    const { complaintId } = req.body
+
+    if (role === 'CUSTOMER') {
+      const user = req.user as Customer
+
+      const complaint = await user.viewComplaint(complaintId)
+
+      return res.status(200).json(complaint)
+    }
+
+    if (role === 'CUSTOMER_SERVICE') {
+      const user = req.user as CustomerService
+
+      const complaint = await user.viewComplaint(complaintId)
+
+      return res.status(200).json(complaint)
+    }
+
+  } catch (e: any) {
+    res.status(400).json({ message: e.message })
+  }
+}
+
+export default viewComplaint
