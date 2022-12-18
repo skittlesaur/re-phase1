0ac2dd5e6560d970b6c0c32a7d717e1c5360a63a
baraa <hey.baraa@gmail.com>
2022-12-18 07:08:33 +0200

    fix build err

--- apps/client/components/home/index.tsx
+++ apps/client/components/home/index.tsx
@@ -6,11 +6,13 @@ const Home = ({ products }: any) => {
       <h1 className="text-2xl">
         Products
       </h1>
-      <div className="grid grid-cols-5 gap-4">
-        {products.map((product: any) => (
-          <Product product={product} key={product.id} />
-        ))}
-      </div>
+      {products && (
+        <div className="grid grid-cols-5 gap-4">
+          {products.map((product: any) => (
+            <Product product={product} key={product.id} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
