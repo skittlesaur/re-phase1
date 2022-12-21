a38afaa682651312b90b88fa3bf912bfb2319fca
baraa <hey.baraa@gmail.com>
2022-12-22 00:06:17 +0200

    format

--- apps/client/pages/login.tsx
+++ apps/client/pages/login.tsx
@@ -1,12 +1,11 @@
-import AppLayout from "@layouts/app";
-import Authentication from "../components/authentication/index";
+import Authentication from '../components/authentication/index'
 
 const LoginPage = () => {
   return (
     <div>
-      <Authentication></Authentication>
+      <Authentication />
     </div>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
