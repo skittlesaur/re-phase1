25de45c0b8083302f013921131a8e642a54d3fcf
baraa <hey.baraa@gmail.com>
2022-12-22 10:14:27 +0200

    domain

--- apps/server/src/controllers/user/register.ts
+++ apps/server/src/controllers/user/register.ts
@@ -17,8 +17,8 @@ const register = async (req: Request, res: Response) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      // maxAge: 1000 * 60 * 60 * 24 * 7,
-      // domain: process.env.CLIENT_URL,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      domain: process.env.CLIENT_URL,
     })
 
     const { password: _, ...userWithoutPassword } = user
