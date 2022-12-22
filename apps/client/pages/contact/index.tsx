a3a982ca2d31b9bf932ea9558f2087e2f765ba77
baraa <hey.baraa@gmail.com>
2022-12-22 17:21:49 +0200

    seo

--- apps/client/pages/contact/index.tsx
+++ apps/client/pages/contact/index.tsx
@@ -1,10 +1,12 @@
 import AppLayout from '@layouts/app'
 import Contact from '@components/contact'
 import EnsureLoggedIn from '@components/ensure-logged-in'
+import SEO from 'ui/seo'
 
 const ContactPage = () => {
   return (
     <AppLayout currentPage="contact">
+      <SEO title="Contact" />
       <EnsureLoggedIn />
       <Contact />
     </AppLayout>
