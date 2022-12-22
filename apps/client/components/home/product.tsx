1ff820a3f2d2d55527276417636fcd4f2d4906ce
baraa <hey.baraa@gmail.com>
2022-12-22 11:46:40 +0200

    ratings

--- apps/client/components/home/product.tsx
+++ apps/client/components/home/product.tsx
@@ -87,7 +87,7 @@ const Product = ({ product }: any) => {
                 onClick={() => handleRating(i + 1)}
                 key={i}
                 onMouseEnter={() => setHoveredStar(i)}
-                className={`cursor-pointer w-4 aspect-square transition-all duration-300 ease-in-out ${hoveredStar !== -1 && hoveredStar < i ? 'opacity-50' : 'opacity-100'} ${i + 1 <= Math.round(product.rating.average) ? `${product.rating?.ids?.includes(user?.id ?? '') ? 'fill-yellow-600' : 'fill-yellow-500'}` : 'fill-gray-600/50'}`}
+                className={`cursor-pointer w-4 aspect-square ${ratingMutation.isLoading ? 'animate-pulse' : ''} transition-all duration-300 ease-in-out ${hoveredStar !== -1 && hoveredStar < i ? 'opacity-50' : 'opacity-100'} ${i + 1 <= Math.round(product.rating.average) ? `${product.rating?.ids?.includes(user?.id ?? '') ? 'fill-yellow-600' : 'fill-yellow-500'}` : 'fill-gray-600/50'}`}
               />
             ))}
           </div>
